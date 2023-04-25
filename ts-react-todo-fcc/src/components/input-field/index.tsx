bc348fd23254f22c8e9de1370ccc087a70eb8c51
anuj <anuj@C40LAP-10090.india.rsystems.com>
2023-04-25 13:16:15 +0530

    ts-apps-practice-01-ts-react-todo-fcc - setup, initial state and props.

---
+++ ts-react-todo-fcc/src/components/input-field/index.tsx
@@ -0,0 +1,40 @@
+import React, { useRef } from 'react'
+
+import './input-field-styles.css'
+
+interface InputFieldProps {
+  todo: string
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+  handleAddTodo: (e: React.FormEvent) => void
+}
+
+const InputField: React.FC<InputFieldProps> = ({ todo, setTodo, handleAddTodo }) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  return (
+    <form
+      className='input'
+      onSubmit={ (e) => {
+          handleAddTodo(e)
+          inputRef.current?.blur()
+        }
+      }
+    >
+      <input
+        ref={inputRef}
+        value={ todo }
+        onChange={ e => setTodo(e.target.value) }
+        type="input"
+        placeholder='Enter a task'
+        className='input__box'
+      />
+
+      <button
+        className="input__submit"
+        type='submit'
+      >Go</button>
+    </form>
+  )
+}
+
+export default InputField
